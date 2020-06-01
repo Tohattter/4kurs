0377a1ac98d246a05bd226e32531a7c275a8a353
Anton <toxattter@mail.ru>
2020-06-01 11:00:54 +0300

    make it better

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function webpackDevMiddleware() {
 }
 
 async function start() {
-  const PORT = config.get('port') || 3000
+  const PORT = process.env.PORT || 3000
  //Поключение к базе данных
   try {
     await mongoose.connect(config.get('mongoURI'), {
@@ -46,9 +46,9 @@ async function start() {
       useFindAndModify: false
     })
 //Сообщение о подключение к серверу и об ошибке если такая есть ! 
-    app.listen(PORT, () => {
-      console.log(`Server started on port: ${PORT}...`)
-    })
+app.listen(process.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
   } catch (e) {
     console.log('Server Error', e.message)
     mongoose.disconnect()
